c2ea9ae27eaa7be2e23668604f90a397f25bfb96
Leo_Amv <leonidamvrosov@yandex.ru>
2022-10-11 23:50:39 +0500

    menu, methods updated, PhoneBook.txt added

--- menu.py
+++ menu.py
@@ -1,3 +1,8 @@
+from methods import create_phone_book, import_data
+import os
+
+
+path = 'PhoneBook.txt'
 print('\n\t\t---Phonebook---\n\nSelect menu item:\n\tImport data: number "1"\n\tExport data: number "2"')
 choise = 0
 while choise < 1 or choise > 2:
@@ -10,6 +15,8 @@ while choise < 1 or choise > 2:
         print('You must enter 1 or 2, try again!')
 
 if choise == 1:
-    print('ok')
+    if os.path.getsize(path) == 0:
+        create_phone_book(path)
+    import_data(path)
 else:
     print('ok')
--- methods.py
+++ methods.py
@@ -1,8 +1,31 @@
-def import_data():
+import os
+
+
+def import_data(path):
+    name = input('Enter Full Name:\n')
+    number = input('Enter phone number:\n')
+    with open(path, 'a+', encoding='utf-8') as pb:
+        pb.writelines(f'\n{name}')
+        pb.write(f'\t\t{number}')
+
+
+def export_data(path):
     print('Enter Full Name:')
     contact = input()
 
 
-def export_data():
-    print('Enter Full Name:')
-    contact = input()
+def create_phone_book(path):
+    with open(path, 'a+', encoding='utf-8') as pb:
+        pb.write('\t\t---Phonebook---\n\n')
+        pb.write('|Full Name|\t\t\t|Phone number|\n')
+
+
+# file = open('file.txt', 'r', encoding='utf-8')
+# file.close
+# if os.path.getsize('file.txt') == 0:
+#     print('empty')
+# else:
+#     print('fill')
+
+
+# print(os.path.getsize('file.txt'))
